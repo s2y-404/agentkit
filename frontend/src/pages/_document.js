323951140e652783acfc1ff9e4a8f1b8692192af
Jackson Anthonipillai <jacksonanthonipillai@MacBook-Pro-de-Jackson-2.local>
2025-03-12 16:00:25 +0100

    add pwa

---
+++ frontend/src/pages/_document.js
@@ -0,0 +1,18 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
