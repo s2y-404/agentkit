7870edfa01fdc13bc46f08622e0146a0afae609a
tanguy.jouvenceau <tanguy.jouvenceau@adventori.com>
2025-03-14 14:42:38 +0100

    frontend optimisation

--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -1,8 +1,7 @@
-export const formatDate = (dateString: string) => {
-  if (!dateString) {
-    return ""
-  }
-
-  const date = new Date(dateString)
-  return new Intl.DateTimeFormat("en-US").format(date)
+const dateFormatter = new Intl.DateTimeFormat("en-US");
+export const formatDate = (dateString: string): string => {
+  if (!dateString) return "";
+  const timestamp = Date.parse(dateString.toString());
+  if(isNaN(timestamp)) return "";
+  return dateFormatter.format(timestamp);
 }
--- frontend/src/utils/id.ts
+++ frontend/src/utils/id.ts
@@ -1,5 +1,3 @@
-import { v4 as uuidv4 } from "uuid"
-
-export const generateUUID = () => {
-  return uuidv4()
-}
+export const generateUUID = (): string => {
+  return crypto.randomUUID();
+};
