356e3e7ea192d2bf2b71d31ec1a0cde71ef3d404
MateoB00 <mateobianco91100@gmail.com>
2025-03-14 14:23:33 +0100

    add loggers for know agent statut

--- backend/app/app/services/chat_agent/helpers/run_helper.py
+++ backend/app/app/services/chat_agent/helpers/run_helper.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from typing import Optional
-
+import logging
 from app.api.deps import get_redis_client
 from app.utils.fastapi_globals import g
 
@@ -9,9 +9,12 @@ async def is_running(run_id: Optional[str] = None) -> bool:
     redis_client = await get_redis_client()
     run_id = run_id or g.query_context["run_id"]
     is_running_bool = await redis_client.get(run_id)
+    if is_running_bool is not None:    
+        logging.info(f'AGENT IS RUNNING {run_id}')
     return is_running_bool is not None
 
 
 async def stop_run(run_id: str) -> None:
     redis_client = await get_redis_client()
+    logging.info(f'AGENT IS CANCELED {run_id}')
     await redis_client.delete(run_id)
